428085cdcf96c0a181785b1c9775c079423b39ae
Tretiakov Stanislav <tretyakovpro4@gmail.com>
2021-01-29 07:24:45 +0300

    stats to dialect

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,23 +3,23 @@ import LanguageContext from '../../contexts/Language'
 
 function Footer () {
   const { languagesStateHelper } = useContext(LanguageContext)
-  const language = languagesStateHelper.getCurrentLanguage()
+  const dialect = languagesStateHelper.getCurrentDialect()
 
   return (
     <footer className="container mx-auto px-4">
       <div className="flex justify-between items-center border-t border-gray-200 py-6 space-x-10">
         <div className="flex-1 space-x-10 text-sm font-medium text-gray-500">
           <span>
-            Словоформ: { language.stats.wordForms }
+            Словоформ: { dialect.stats.wordForms }
           </span>  
           <span>
-            Лексем: { language.stats.tokens }
+            Лексем: { dialect.stats.tokens }
           </span>  
           <span>
-            Предложений: { language.stats.sentences }
+            Предложений: { dialect.stats.sentences }
           </span>
           <span>
-            Текстов: { language.stats.texts }
+            Текстов: { dialect.stats.texts }
           </span>
         </div>
         {/* <div className="flex space-x-4">
--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -7,31 +7,27 @@ function fetchInitialData() {
         id: 1,
         title: 'Осетинский',
         dialects: [
-          { id: 1, title: 'Иронский' },
-          { id: 2, title: 'Дигорский' }
-        ],
-        stats: {
-          wordForms: 100,
-          sentences: 56,
-          tokens: 134,
-          texts: 2
-        }
-      },
-      {
-        id: 2,
-        title: 'Узбекский',
-        dialects: [
-          { id: 3, title: 'Хива' },
-          { id: 4, title: 'Фергана' },
-          { id: 5, title: 'Ташкент' },
-          { id: 6, title: 'Самарканд-Бухара' }
-        ],
-        stats: {
-          wordForms: 2177,
-          sentences: 360,
-          tokens: 1030,
-          texts: 70
-        }
+          {
+            id: 1,
+            title: 'Иронский',
+            stats: {
+              wordForms: 2177,
+              sentences: 360,
+              tokens: 1030,
+              texts: 70
+            }
+          },
+          {
+            id: 2,
+            title: 'Дигорский',
+            stats: {
+              wordForms: 100,
+              sentences: 56,
+              tokens: 134,
+              texts: 2
+            }
+          }
+        ]
       }
     ]
   }
