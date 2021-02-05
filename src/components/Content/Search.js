16169d52e085276e05b38daa1c7b4d977f8aceb6
Tretiakov Stanislav <tretyakovpro4@gmail.com>
2021-02-05 09:11:15 +0300

    Added descriptions to search column

--- src/components/Content/Search.js
+++ src/components/Content/Search.js
@@ -1,19 +1,22 @@
 import React, { useState } from 'react'
 import * as ApiService from '../../services/ApiService'
 import { IconContext } from 'react-icons'
-import { FaSearch } from 'react-icons/fa'
+import { FaSearch, FaCaretUp, FaRegFrown } from 'react-icons/fa'
 
 function Search () {
   const [ wordFormList, setWordFormList ] = useState([])
+  const [ query, setQuery ] = useState(null)
 
   function searchWordForms(query) {
     if (!query) {
       setWordFormList([])
+      setQuery(null)
       return
     }
 
     const items = ApiService.searchWordForms(query)
     setWordFormList(items)
+    setQuery(query)
   }
 
   return (
@@ -22,7 +25,7 @@ function Search () {
         <input
           onChange={e => searchWordForms(e.target.value)}
           className="border-b border-gray-200 h-14 w-full pl-6 pr-12 rounded-sm rounded-b-none text-base focus:outline-none"
-          placeholder="Быстрый поиск..."
+          placeholder="Начните вводить слово..."
           type="input"
         />
 
@@ -32,6 +35,28 @@ function Search () {
       </div>
 
       <div className="absolute left-0 right-0 top-14 bottom-0 overflow-y-auto">
+        {!wordFormList.length && !query && (
+          <div className="text-gray-400 text-center mt-40">
+            <div className="flex justify-center mb-2">
+              <IconContext.Provider value={{ className: "text-gray-300 text-2xl" }}>
+                <FaCaretUp />
+              </IconContext.Provider>
+            </div>
+            Используйте строку поиска для<br /> получения списка слов
+          </div>
+        )}
+
+        {!wordFormList.length && query && (
+          <div className="text-gray-400 text-center mt-40">
+            <div className="flex justify-center mb-2">
+              <IconContext.Provider value={{ className: "text-gray-300 text-2xl" }}>
+                <FaRegFrown />
+              </IconContext.Provider>
+            </div>
+            По вашему запросу ничего не найдено.<br /> Проверьте ввод на опечатки.
+          </div>
+        )}
+
         {wordFormList.map(item => {
           return (
             <div className="flex relative transform scale-100 text-xs py-1 px-6 h-12 items-center hover:bg-gray-100" key={item.id}>
